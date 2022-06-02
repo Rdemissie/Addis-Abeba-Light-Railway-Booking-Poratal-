77f717598322361c3f67c2479e09b7be533257b5
Rdemissie <33603291+Rdemissie@users.noreply.github.com>
2022-06-02 15:30:34 +0300

    Add files via upload

---
+++ AALRWTP/book.php
@@ -0,0 +1,168 @@
+<?php
+include 'connect.php' ;
+session_start();
+if ($_SESSION['log'] == '')
+{
+    header("location:sindex.php");
+}
+include 'header.php';
+?>
+
+<style>
+
+    .container{
+        border-spacing: 10px;
+
+      font-family: Montserrat, sans-serif;
+     font-size: 18px !important;
+      border: 2px solid grey;
+        margin-top: 30px;
+        margin-bottom: 50px;
+       padding-top: 50px;
+      padding-right: 50px;
+      padding-bottom: 50px;
+      padding-left: 150px;
+      align-content: center;
+    }
+    .button {
+  padding: 15px 32px;
+  align-content: left;
+  color: white;
+background-color:black;
+    }
+#number {
+  overflow: hidden;
+  width: 600px;
+}
+input[type=number]{
+    width: 250px;
+} 
+  </style>
+
+
+  </script>
+</head>
+<body>
+  <h1><center><b>Train Ticket Booking &nbsp</center></b></h1>
+ <form method='post' action ='book_action.php' align='center'>
+
+
+
+
+
+
+<div class="container">
+<section id="form" class="formborder">
+<div class="container2">
+       <form>
+        
+
+
+       <div class="form-row row justify-content-around" name="source">
+  &nbsp
+
+       <div class="form-group col-md-5" name="source">
+       <label for="inputEmail4">SOURCE STATION :</label>    
+       <select id="inputState" class="form-control" name="source">
+   <option>Torhailoch</option>
+   <option>Lideta</option>
+   <option>Mexico Adebabay</option>
+   <option>Legehar</option>
+   <option>Stadium</option>
+   <option>Bambis</option>
+   <option>Urael Church</option>
+   <option>Hayahulet 2</option>
+   <option>Hayahulet 1</option>
+   <option>Leme Hotel</option>
+   <option>Megenagna</option>
+   <option>Gurd Shola 2</option>
+   <option>Gurd Shola 1</option>
+   <option>Civil Service University</option>
+   <option>CMC</option> 
+   <option>Aayat</option>
+   </select>
+
+
+     <!-- <input type="email" class="form-control" id="inputEmail4" placeholder="FROM">-->
+    </div>             
+
+
+   
+
+
+
+          <div class="form-group col-md-5" name="dest">
+      <label for="inputPassword4"> 
+FINAL DESTINATION :</label>
+      <select id="inputState" class="form-control" name="dest">
+   
+   
+   <option>Lideta</option>
+   <option>Mexico Adebabay</option>
+   <option>Legehar</option>
+   <option>Stadium</option>
+   <option>Bambis</option>
+   <option>Urael Church</option>
+   <option>Hayahulet 2</option>
+   <option>Hayahulet 1</option>
+   <option>Leme Hotel</option>
+   <option>Megenagna</option>
+   <option>Gurd Shola 2</option>
+   <option>Gurd Shola 1</option>
+   <option>Civil Service University</option>
+   <option>CMC</option> 
+   <option>Aayat</option>
+   </select> 
+
+      <!--<input type="password" class="form-control" id="inputPassword4" placeholder="TO">-->
+  </div>
+  
+ <center>  
+         <!--  <div class="form-group col-md-5" name="class">
+      <label for="inputPassword4"> 
+Train Class :</label>
+      <select id="inputState" class="form-control" name="class">
+   
+  <option>First</option>
+  <option>Second</option>
+   <option>A.C</option>
+    -->
+   </select> 
+
+  </div>
+ 
+<!--  <center><div class="form-group col-md-5" name="type">
+    <label for="inputState">JOURNEY TYPE :</label>
+    <select id="inputState" class="form-control" name="type">
+    <option >Single</option>
+    <option >Return</option>
+    </select>
+    </div> -->
+   
+    
+      <br>
+    <div class="form-row row justify-content-aroundd">
+       <div class="form-group col-md-16" >
+       <center><label for="inputState"  ><h8>NO OF PASSENGERS :</h8></label></center>
+      <center> <input type="number" name="number" required  min="1" max="5"  ></center>
+       </div>
+
+    
+
+    <div>   
+     <button type="submit"    class="button" name="login_submit" >Proceed</button>
+    
+ </div>
+</form>
+</div>
+
+</section>
+</div>
+  
+
+
+<?php include 'footer.php';
+?> 
+
+</body>
+</html>
---
+++ AALRWTP/footer.php
@@ -0,0 +1,45 @@
+
+<head>
+  <style>
+
+footer {
+
+  
+    font-family: Montserrat, sans-serif;
+    margin-bottom: 0;
+    background-color: #2d2d30;
+    border: 0;
+    font-size: 18px !important;
+    letter-spacing: 2px;
+    opacity: 5;
+    padding: 10px;
+    border-radius: 0;
+
+    }
+  </style>
+<body>
+  <br>
+  <br>
+  <br>
+  <br>
+<br>
+  <br>
+<br>
+  <br>
+<br>
+  <br>
+
+<footer class="text-center">
+ 
+
+  
+<div align="right"></div>
+ <a href="#" style="color: white; text-decoration: none"></a>&nbsp&nbsp&nbsp
+   <a href="#" style="color: white; text-decoration: none"></a>
+    <br>
+  <h2><img src="copy.ico" width="20px"><a style="color: white; text-decoration: none">&nbspAddis Abeba Light Railway Ticket Booking Portal</a></h2>
+
+
+</div>
+</footer>
+</body></head>
